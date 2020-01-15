6e3ff44c709e92ec65418dd75942987f095b2d04
Doris Le <dorisle@sandiego.edu>
2020-01-15 04:14:57 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,3 +6,11 @@ function destructivelyUpdateObjectWithKeyAndValue(obj, key, value) {
   obj[key] = value
   return obj
 }
+
+function deleteFromObjectByKey(object, key) {
+
+}
+
+function destructivelyDeleteFromObjectByKey(obj, key){
+  
+}
